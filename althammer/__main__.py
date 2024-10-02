ff6a5040ea9d06888a6cf93bd42b0df588918e61
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-01 23:10:49 -0400

    Update __main__.py

--- althammer/__main__.py
+++ althammer/__main__.py
@@ -31,9 +31,10 @@ def before_request():
 def after_request(resp):
 
     #script nonce
-    nonce=generate_hash(f"{session.get('session_id')}+{g.time}")
+    #nonce=generate_hash(f"{session.get('session_id')}+{g.time}")
     
-    resp.headers["Content-Security-Policy"] = f"default-src * data:; script-src 'self' code.jquery.com cdn.jsdelivr.net 'nonce-{nonce}'; object-src 'none'; style-src 'self' 'nonce-{nonce}' cdn.jsdelivr.net; media-src 'none';"
+    #resp.headers["Content-Security-Policy"] = f"default-src * data:; script-src 'self' code.jquery.com cdn.jsdelivr.net 'nonce-{nonce}'; object-src 'none'; style-src 'self' 'nonce-{nonce}' cdn.jsdelivr.net; media-src 'none';"
+    resp.headers["Content-Security-Policy"] = f"default-src * data:; script-src 'self' code.jquery.com cdn.jsdelivr.net; object-src 'none'; style-src 'self'cdn.jsdelivr.net; media-src 'none';"
     resp.headers["Cross-Origin-Opener-Policy"] = "same-origin"
     resp.headers["Cross-Origin-Resource-Policy"] = "same-origin"
     resp.headers["Permissions-Policy"] = "geolocation=(self)"
