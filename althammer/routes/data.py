7681a3a6159309bdbcdd8f924d369891566053a5
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-02 20:25:09 -0400

    f

--- althammer/routes/data.py
+++ althammer/routes/data.py
@@ -1,2 +1,8 @@
 from althammer.__main__ import app
 
+@app.get("/faction/<faction>"):
+def faction_faction(faction):
+
+    f = get_faction(faction)
+
+    return render_template("faction.html", f=f)
