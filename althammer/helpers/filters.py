64cab025f020d769f0e85fcaa59c5abebae8774c
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-20 10:54:51 -0400

    Update filters.py

--- althammer/helpers/filters.py
+++ althammer/helpers/filters.py
@@ -92,5 +92,7 @@ def keyword(x):
     text=markdown_filter(text)
 
     text = text.replace('"', r'\"')
+    text = text.replace('<', r'&lt;')
+    text = text.replace('>', r'&gt;')
 
     return f'<span type="button" class="text-nowrap" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="click hover focus" data-bs-title="{kwd}" data-bs-content="{text}">{x}</span>'
