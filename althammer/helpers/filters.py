6de393052870411d93dec65dbde6d6613c7eccca
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-20 10:26:05 -0400

    Update filters.py

--- althammer/helpers/filters.py
+++ althammer/helpers/filters.py
@@ -36,7 +36,7 @@ def keyword(x):
 
     kwd, text = get_keyword(x)
 
-    return f'<a type="button" class="text-nowrap" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="click hover focus" data-bs-title="{kwd}" data-bs-content="{text}">{x}</a>'
+    return f'<span type="button" class="text-nowrap" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="click hover focus" data-bs-title="{kwd}" data-bs-content="{text}">{x}</span>'
 
 
 @app.template_filter("listcomp")
