45cb8cae31b33e965482e915c2009486108b8cbc
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-20 10:09:19 -0400

    Update site.js

--- althammer/assets/js/site.js
+++ althammer/assets/js/site.js
@@ -159,6 +159,10 @@ $("#dark-mode-toggle").click(function(){
 })
 
 //Initialize popovers
-$('[data-bs-toggle="popover"]').popover()
+$(document).ready(
+  function() {
+    $('[data-bs-toggle="popover"]').popover()
+  }
+)
 //const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 //const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
