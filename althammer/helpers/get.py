5f297100ade0f8b4620b0108820643fd62b4a9d9
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-20 10:41:18 -0400

    more keywords

--- althammer/helpers/get.py
+++ althammer/helpers/get.py
@@ -29,7 +29,7 @@ def get_factions():
 @cache.memoize()
 def get_keyword(keyword):
 
-    with open("althammer/data/weapon_keywords.json", "r+") as file:
+    with open("althammer/data/keywords.json", "r+") as file:
         data=json.load(file)
 
     if keyword in data.keys():
@@ -38,8 +38,8 @@ def get_keyword(keyword):
     elif keyword.startswith("Anti-"):
         return "Anti-Keyword X+", data["Anti-Keyword X+"]
 
-    elif re.search(r"\d$", keyword):
-        keyword=re.sub(r"\d$", "X", keyword)
+    elif re.search(r"\d", keyword):
+        keyword=re.sub(r"\d", "X", keyword)
         return keyword, data[keyword]
 
     else:
