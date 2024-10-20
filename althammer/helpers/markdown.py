31b53cd3ef1b7ee4fd7679813d2f03d461f70f2f
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-20 17:40:07 -0400

    Update markdown.py

--- althammer/helpers/markdown.py
+++ althammer/helpers/markdown.py
@@ -66,8 +66,13 @@ class CustomRenderer(HTMLRenderer):
 
         if kwd=="Error":
             return f'<span class="keyword">{token.target}</span>'
-        else:
-            return f'<span type="button" class="keyword text-nowrap" data-bs-toggle="popover" data-bs-html="true" data-bs-placement="bottom" data-bs-trigger="hover" data-bs-title="{kwd}" data-bs-content="{rule}">{token.target}</span>'
+        
+
+        rule=self.render_inner(rule)
+        rule=rule.replace('"', '&quot;')
+        kwd = kwd.replace('"', '&quot;')
+
+        return f'<span type="button" class="keyword text-nowrap" data-bs-toggle="popover" data-bs-html="true" data-bs-placement="bottom" data-bs-trigger="hover" data-bs-title="{kwd}" data-bs-content="{rule}">{token.target}</span>'
 
     def render_keyword_alt(self, *args, **kwargs):
         return self.render_keyword(*args, **kwargs)
