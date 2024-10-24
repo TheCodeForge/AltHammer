5dfc2693d2f427fb8c057272ffe65cd030fbe8cb
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-23 22:08:11 -0400

    fix

--- althammer/assets/js/site.js
+++ althammer/assets/js/site.js
@@ -204,5 +204,3 @@ $(document).ready(
     $('[data-bs-toggle="popover"]').popover()
   }
 )
-//const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-//const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
