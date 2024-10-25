5c8d838e572e21d50918bcd1bf0d30cb0814a3bc
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-25 09:04:34 -0400

    Update core.py

--- althammer/classes/core.py
+++ althammer/classes/core.py
@@ -46,29 +46,41 @@ class Unit(Base):
 
         try:
             if self.__dict__.get('models_max'):
+                print(f"hard set models max {self.__dict__.get('models_max')}")
                 max_unit_size= self.models_max
             elif any([x in self.keywords for x in ["Character","Monster","Vehicle","Epic Hero"]]):
+                print('keyword set max 1')
                 max_unit_size= 1
 
             elif self.profiles:
+                print('profiles detected')
                 for p in self.profiles:
+                    print(f'profile: {p.name}')
                     if any([x in p.keywords for x in ["Character","Monster","Vehicle","Epic Hero"]]):
+                        print('profile keyword set max 1')
                         max_unit_size=1
 
             else:
+                print('default max 100')
                 max_unit_size = 100
 
             if "Battleline" in self.keywords:
+                print(f'Battleline: max 6x')
                 return max_unit_size * 6
             elif "Epic Hero" in self.keywords:
+                print(f'Epic Hero: max 1x')
                 return max_unit_size
 
             elif self.profiles:
+                print('profiles detected')
                 for p in self.profiles:
+                    print(f'profile: {p.name}')
                     if any([x in p.keywords for x in ["Epic Hero"]]):
+                        print('profile epic hero max 1')
                         return max_unit_size
 
             else:
+                print('default max 3x')
                 return max_unit_size * 3
 
         except Exception as e:
