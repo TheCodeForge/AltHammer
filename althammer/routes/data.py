d2e8b0d7b560b976f5add216177bce7b20575a69
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-10-26 12:30:08 -0400

    Update data.py

--- althammer/routes/data.py
+++ althammer/routes/data.py
@@ -26,6 +26,6 @@ def faction_faction_unit_unit(faction, unit):
 
     f=get_faction(faction)
 
-    u = f.unit(unit)
+    u = f.unit(unit, id=unit)
 
     return render_template("unit.html", f=f, u=u)
