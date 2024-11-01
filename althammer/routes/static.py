0570732354c4a665a29655d9efcb99109d9981d7
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-11-01 19:55:47 -0400

    Update static.py

--- althammer/routes/static.py
+++ althammer/routes/static.py
@@ -27,7 +27,7 @@ def light_css(stylefile, color=None):
         scss=stylesheet.read()
         scss=scss.replace('{primary}', app.config['COLOR_PRIMARY'])
         scss=scss.replace('{secondary}', app.config['COLOR_SECONDARY'])
-        scss=scss.replace('{faction}', color or request.args.get('color') or '')
+        scss=scss.replace('{faction}', color or '')
 
         css=sass.compile(string=scss)
 
