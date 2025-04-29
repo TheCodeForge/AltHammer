f5bc02891ec8e6dfb62d64312c79e7a8d92badf7
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2025-04-28 21:05:06 -0400

    Update lazy.py

--- althammer/helpers/lazy.py
+++ althammer/helpers/lazy.py
@@ -8,13 +8,15 @@ def lazy(f):
 
         o = args[0]
 
+        key=f"{f.__name__}|{[str(x) for x in args[1:]]}|{sorted([str(x)+"="+str(kwargs[x]) for x in kwargs])}"
+
         if "_lazy" not in o.__dict__:
             o.__dict__["_lazy"] = {}
 
-        if f.__name__ not in o.__dict__["_lazy"]:
-            o.__dict__["_lazy"][f.__name__] = f(*args, **kwargs)
+        if key not in o.__dict__["_lazy"]:
+            o.__dict__["_lazy"][key] = f(*args, **kwargs)
 
-        return o.__dict__["_lazy"][f.__name__]
+        return o.__dict__["_lazy"][key]
 
     wrapper.__name__ = f.__name__
     return wrapper
